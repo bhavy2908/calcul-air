5cdedf0a8a8993e4647ff3f12bfdb575bba95be2
Bhavy <bhavy2908@gmail.com>
2021-06-01 04:12:58 +0530

    Added Finger Counter

---
+++ FingerCounter.py
@@ -0,0 +1,45 @@
+import cv2
+import Tracker as Tracker
+
+wCam, hCam = 1280, 720
+
+cap = cv2.VideoCapture(0)
+cap.set(3, wCam)
+cap.set(4, hCam)
+
+handDetector = Tracker.handDetector()
+
+fingerTips = [8, 12, 16, 20]
+while True:
+    success, img = cap.read()
+
+    img = handDetector.detectHands(img, display=False)
+    landmarkList = handDetector.findPosition(img)
+    if len(landmarkList) != 0:
+        fingerCount = []
+        if landmarkList[4][1] > landmarkList[17][1]:
+            rightHand = False
+            if landmarkList[3][1] < landmarkList[4][1]:
+                fingerCount.append(1)
+            else:
+                fingerCount.append(0)
+
+        else:
+            rightHand = True
+            if landmarkList[4][1] < landmarkList[2][1]:
+                fingerCount.append(1)
+            else:
+                fingerCount.append(0)
+        for id in range(0, 4):
+            if landmarkList[fingerTips[id]][2] < landmarkList[fingerTips[id]-1][2]:
+                fingerCount.append(1)
+            else:
+                fingerCount.append(0)
+        totalFingers = fingerCount.count(1)
+        cv2.putText(img, "Fingers: " + str(int(totalFingers)), (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1.5, (255, 255, 255), 2)
+
+    else:
+        cv2.putText(img, "No Hands Found", (10, 50), cv2.FONT_HERSHEY_DUPLEX, 1.5, (255, 255, 255),
+                    2)
+    cv2.imshow("Calcul Air", img)
+    cv2.waitKey(1)
--- Tracker.py
+++ Tracker.py
@@ -16,13 +16,27 @@ class handDetector():
 
     def detectHands(self, img, display=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        results = self.hands.process(imgRGB)
+        self.results = self.hands.process(imgRGB)
 
-        if results.multi_hand_landmarks:
-            for handLandmarks in results.multi_hand_landmarks:
+        if self.results.multi_hand_landmarks:
+            for handLandmarks in self.results.multi_hand_landmarks:
                 if display:
                     self.drawHand.draw_landmarks(img, handLandmarks, self.mpHands.HAND_CONNECTIONS)
         return img
+
+    def findPosition(self, img, handNo=0):
+
+        landmarkList = []
+
+        if self.results.multi_hand_landmarks:
+            hand = self.results.multi_hand_landmarks[handNo]
+            for id, lm in enumerate(hand.landmark):
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+
+                landmarkList.append([id, cx, cy])
+
+        return landmarkList
 
 
 def main():
@@ -33,11 +47,13 @@ def main():
     while True:
         success, img = cap.read()
         img = hand_detector.detectHands(img)
+        landmarkList = hand_detector.findPosition(img)
+
         currentTime = time.time()
         fps = 1 / (currentTime - previousTime)
         previousTime = currentTime
 
-        #cv2.putText(img, str(int(fps)), (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
+        cv2.putText(img, str(int(fps)), (10, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 3)
 
         cv2.imshow("Calcul Air", img)
         cv2.waitKey(1)
